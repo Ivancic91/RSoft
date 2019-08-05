d77d14a63f1b2a78a00e118f64bd9ceef933842a
Robert Ivancic <ivancicr@rrlogin.internal>
2019-08-04 23:11:15 -0400

    added sphinx directory into docs and added location of RSoftTools documentation to readme part 7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,16 +12,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('./../../RSoftPython/'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'RSoft'
-copyright = '2019, Robert Ivancic'
-author = 'Robert Ivancic'
+project = 'RSoftPython'
+copyright = '2019, Robert J. S. Ivancic'
+author = 'Robert J. S. Ivancic'
 
 # The short X.Y version
 version = ''
@@ -40,13 +40,11 @@ release = '1.0'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.todo',
+    'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.autosummary'
-    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -82,7 +80,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -109,7 +107,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'RSoftdoc'
+htmlhelp_basename = 'RSoftPythondoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -136,8 +134,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'RSoft.tex', 'RSoft Documentation',
-     'Robert Ivancic', 'manual'),
+    (master_doc, 'RSoftPython.tex', 'RSoftPython Documentation',
+     'Robert J. S. Ivancic', 'manual'),
 ]
 
 
@@ -146,7 +144,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'rsoft', 'RSoft Documentation',
+    (master_doc, 'rsoftpython', 'RSoftPython Documentation',
      [author], 1)
 ]
 
@@ -157,8 +155,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'RSoft', 'RSoft Documentation',
-     author, 'RSoft', 'One line description of project.',
+    (master_doc, 'RSoftPython', 'RSoftPython Documentation',
+     author, 'RSoftPython', 'One line description of project.',
      'Miscellaneous'),
 ]
 
@@ -182,8 +180,3 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
-
-# -- Options for todo extension ----------------------------------------------
-
-# If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
